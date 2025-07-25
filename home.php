b30bd31f6d5323de2fded15160dadf9d0414abcd
Your Name <carljosephsumagang17@gmail.com>
2025-07-25 21:48:39 +0800

    Partial Implementation

--- home.php
+++ home.php
@@ -1,19 +1,16 @@
-<?php 
-  require('./session.php');
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <span><strong>WELCOME MGA BOSS!</strong></span>
-  <form action="/php-login-with-session/logout.php" method="post">
-    <input type="submit" value="LOGOUT" />
-  </form>
+    <div>
+        <section>header</section>
+        <ul>
+            <li><a href="">Home</a></li>
+        </ul>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-<?php 
-  
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <span><strong>WELCOME</strong></span>
-  <form action="/php-login-with-session/logout.php" method="post">
-    <input type="submit" value="LOGOUT" />
-  </form>
+    <div>
+        <section>header</section>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li></li>
+        </ul>
+    </div>
 </body>
 </html>
